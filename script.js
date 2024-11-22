4d85638827dd8497531f68eff6254ec11d562dec
jinxojin <kepsee@icloud.com>
2024-11-22 00:12:03 +0000

    added functions to JS that adds book to library which is an array

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+const myLibrary = [];
+
 function Book(title, author, pages, hasRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.hasRead = hasRead;
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? "finished reading." : "hasn't read yet."}`;
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.hasRead = hasRead;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.hasRead ? "finished reading." : "hasn't read yet."
+    }`;
+  };
 }
 
-const theAlchemist = new Book("The Alchemist", "Paulu Coelho", "84", true)
-console.log(theAlchemist.info());
+function addBookToLibrary(title, author, pages, hasRead) {
+  myLibrary.push(new Book(title, author, pages, hasRead));
+}
+
+addBookToLibrary("The Alchemist", "Paulu Coelho", "84", true);
+console.log(myLibrary[0].info());
